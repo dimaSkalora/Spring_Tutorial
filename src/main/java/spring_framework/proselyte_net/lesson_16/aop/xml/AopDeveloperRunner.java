2fe633d2123bab6b0aa472da934c4a64151fb54c
dimaSkalora <timon2@ukr.net>
2017-12-07 20:36:57 +0200

    16. АОП в Spring Framework (AOP). XML

---
+++ src/main/java/spring_framework/proselyte_net/lesson_16/aop/xml/AopDeveloperRunner.java
@@ -0,0 +1,15 @@
+package spring_framework.proselyte_net.lesson_16.aop.xml;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AopDeveloperRunner {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("SpringFramework/proselyte_net/Lesson_16/aop-developer-config.xml");
+        Developer developer = (Developer) context.getBean("developer");
+
+        System.out.println(developer);
+        developer.throwSomeMysticException();
+    }
+}
---
+++ src/main/java/spring_framework/proselyte_net/lesson_16/aop/xml/Logging.java
@@ -0,0 +1,20 @@
+package spring_framework.proselyte_net.lesson_16.aop.xml;
+
+public class Logging {
+    public void beforeAdvice() {
+        System.out.println("Now we are going to initiate developer's profile.");
+    }
+
+    public void afterAdvice() {
+        System.out.println("Developer's profile has been initiated.");
+    }
+
+    public void afterReturningAdvice(Object someValue) {
+        System.out.println("Value: " + someValue.toString());
+    }
+
+    public void inCaseOfExceptionThrowAdvice(ClassCastException e) {
+        System.out.println("We have an exception here: " + e.toString());
+    }
+
+}
