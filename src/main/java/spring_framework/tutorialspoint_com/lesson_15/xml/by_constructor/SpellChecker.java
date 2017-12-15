f20bda724237852c10439d1b6693edd112b29f23
dimaSkalora <timon2@ukr.net>
2017-12-15 19:02:09 +0200

    Spring - Beans Auto-Wiring. Constructor

---
+++ src/main/java/spring_framework/tutorialspoint_com/lesson_15/xml/by_constructor/SpellChecker.java
@@ -0,0 +1,10 @@
+package spring_framework.tutorialspoint_com.lesson_15.xml.by_constructor;
+
+public class SpellChecker {
+    public SpellChecker(){
+        System.out.println("Inside SpellChecker constructor." );
+    }
+    public void checkSpelling() {
+        System.out.println("Inside checkSpelling." );
+    }
+}
---
+++ src/main/java/spring_framework/tutorialspoint_com/lesson_15/xml/by_constructor/TextEditor.java
@@ -0,0 +1,22 @@
+package spring_framework.tutorialspoint_com.lesson_15.xml.by_constructor;
+
+public class TextEditor {
+    private SpellChecker spellChecker;
+    private String name;
+
+    public TextEditor(SpellChecker spellChecker, String name) {
+        this.spellChecker = spellChecker;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    // a getter method to return spellChecker
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+    public void spellCheck() {
+        spellChecker.checkSpelling();
+    }
+}
