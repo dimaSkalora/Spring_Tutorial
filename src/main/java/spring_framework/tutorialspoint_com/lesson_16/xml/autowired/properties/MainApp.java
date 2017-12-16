80ac6d4d9c744a5e7bca2c257b1a9fcdb2a8afa0
dimaSkalora <timon2@ukr.net>
2017-12-16 17:20:06 +0200

    Spring - Annotation Based Configuration. Autowired - Properties

---
+++ src/main/java/spring_framework/tutorialspoint_com/lesson_16/xml/autowired/properties/MainApp.java
@@ -0,0 +1,12 @@
+package spring_framework.tutorialspoint_com.lesson_16.xml.autowired.properties;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("SpringFramework/tutorialspoint_com/Lesson_16/autowiredProperties-beans.xml");
+        TextEditor te = (TextEditor) context.getBean("textEditor");
+        te.spellCheck();
+    }
+}
