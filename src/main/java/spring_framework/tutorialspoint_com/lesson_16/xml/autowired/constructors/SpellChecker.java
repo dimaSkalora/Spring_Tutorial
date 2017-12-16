1805bbb6f374719f4db90d4913d3134ec953507d
dimaSkalora <timon2@ukr.net>
2017-12-16 17:19:12 +0200

    Spring - Annotation Based Configuration. Autowired - Constructor

---
+++ src/main/java/spring_framework/tutorialspoint_com/lesson_16/xml/autowired/constructors/SpellChecker.java
@@ -0,0 +1,10 @@
+package spring_framework.tutorialspoint_com.lesson_16.xml.autowired.constructors;
+
+public class SpellChecker {
+    public SpellChecker(){
+        System.out.println("Inside SpellChecker constructor." );
+    }
+    public void checkSpelling(){
+        System.out.println("Inside checkSpelling." );
+    }
+}
