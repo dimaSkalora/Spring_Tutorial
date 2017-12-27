a3713341b6bc44c6b0b6051a3a7d1aaafbd03a21
dimaSkalora <timon2@ukr.net>
2017-12-27 10:20:45 +0200

    14 - Spring Beans AutoWiring. XML. byName

---
+++ src/main/java/spring_framework/wideskills_com/lesson_14/xml/byName/Student.java
@@ -0,0 +1,11 @@
+package spring_framework.wideskills_com.lesson_14.xml.byName;
+
+public class Student {
+    private String name ;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
