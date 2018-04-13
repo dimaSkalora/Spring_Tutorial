8670c084c6de62dc77ef7e08d3200717165d5a71
dimaSkalora <timon2@ukr.net>
2018-04-13 17:28:39 +0300

    Spring MVC - Text Box Example(tutorialspoint.com)

---
+++ src/main/WebApp/SpringMVC/tutorialspoint_com/Spring_MVC_Form_Tag_Library/Text_Box/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>SpringMVC-tutorialspoint_com</title>
+</head>
+<body>
+<h2>SpringMVC-tutorialspoint_com-Spring_MVC_Form_Tag_Libraryg-Text Box</h2>
+    <br>
+    <a href="http://localhost:8080/SpringMVC/tutorialspoint_com/Form_Tag_Library/textBox/student">Text Box</a><br>
+    <a href="http://localhost:8080/student">Text Box</a><br>
+
+</body>
+</html>
---
+++ src/main/java/spring_mvc/tutorialspoint_com/spring_mvc_form_tag_library/text_box/Student.java
@@ -0,0 +1,28 @@
+package spring_mvc.tutorialspoint_com.spring_mvc_form_tag_library.text_box;
+
+public class Student {
+    private Integer age;
+    private String name;
+    private Integer id;
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getId() {
+        return id;
+    }
+}
