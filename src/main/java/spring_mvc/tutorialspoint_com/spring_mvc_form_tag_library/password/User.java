f966b294805131ec73d18132a30c7bccc536ecf1
dimaSkalora <timon2@ukr.net>
2018-04-14 13:00:57 +0300

    Spring MVC - Password Example(tutorialspoint.com)

---
+++ src/main/java/spring_mvc/tutorialspoint_com/spring_mvc_form_tag_library/password/User.java
@@ -0,0 +1,20 @@
+package spring_mvc.tutorialspoint_com.spring_mvc_form_tag_library.password;
+
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
