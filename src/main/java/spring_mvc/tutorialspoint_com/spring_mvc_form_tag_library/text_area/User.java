3ece11fcef1e3a8478ecb75c79515a96c085b8b2
dimaSkalora <timon2@ukr.net>
2018-04-16 17:19:05 +0300

    Spring MVC - TextArea Example(tutorialspoint.com)

---
+++ src/main/java/spring_mvc/tutorialspoint_com/spring_mvc_form_tag_library/text_area/User.java
@@ -0,0 +1,27 @@
+package spring_mvc.tutorialspoint_com.spring_mvc_form_tag_library.text_area;
+
+public class User {
+    private String username;
+    private String password;
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
