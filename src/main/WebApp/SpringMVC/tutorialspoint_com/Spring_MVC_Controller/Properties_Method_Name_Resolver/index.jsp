6ca99058d1cdff6a34b3f1d8f513905293e6e9fc
dimaSkalora <timon2@ukr.net>
2018-04-21 22:55:28 +0300

    Spring MVC - Properties Method Name Resolver Example(tutorialspoint.com)

---
+++ src/main/WebApp/SpringMVC/tutorialspoint_com/Spring_MVC_Controller/Properties_Method_Name_Resolver/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>SpringMVC-tutorialspoint_com</title>
+</head>
+<body>
+<h2>SpringMVC-tutorialspoint_com-Spring_MVC_Controller - Parameter  Method Name Resolver</h2>
+    <br>
+
+    <a href = "user/home.htm" >Home</a> <br>
+    <a href = "user/add.htm" >Add</a> <br>
+    <a href = "user/remove.htm" >Remove</a>
+
+</body>
+</html>
---
+++ src/main/java/spring_mvc/tutorialspoint_com/spring_mvc_controller/properties_method_name_resolver/UserController.java
@@ -0,0 +1,28 @@
+package spring_mvc.tutorialspoint_com.spring_mvc_controller.properties_method_name_resolver;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserController extends MultiActionController {
+
+    public ModelAndView home(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        ModelAndView modelAndView = new ModelAndView("user");
+        modelAndView.addObject("message", "Home");
+        return modelAndView;
+    }
+
+    public ModelAndView add(HttpServletRequest request, HttpServletResponse response)throws Exception{
+        ModelAndView modelAndView = new ModelAndView("user");
+        modelAndView.addObject("message","Add");
+        return modelAndView;
+    }
+
+    public ModelAndView remove(HttpServletRequest request, HttpServletResponse response)throws Exception{
+        ModelAndView modelAndView = new ModelAndView("user");
+        modelAndView.addObject("message", "Remove");
+        return modelAndView;
+    }
+}
